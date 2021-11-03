8f6acb73e5c4b3669328e3dcff3145221df18d49
Valeriu1 <roscavaleriuvaleriu@gmail.com>
2021-11-03 22:33:10 +0100

    Client

---
+++ Data/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Assigment1.Data
+{
+    public interface IUserService
+    {
+        Task<User> ValidateUser(string userName, string password);
+    }
+}
